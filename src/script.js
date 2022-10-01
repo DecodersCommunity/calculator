5cb9936f5fdda85042a02a73ec1b769c5ba98a85
Shradha <shradha16.edu@gmail.com>
2022-10-01 14:58:19 +0530

    created calculator using html css javascript

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
 
+var val,a;
+function show(num)
+{
+document.getElementById("main").value+=num;
+}
+function answer(){
+  z=document.getElementById("main").value;
+  document.getElementById("main").value=eval(z)
+}
+function erase()
+{
+  document.getElementById("main").value=" ";
+}
