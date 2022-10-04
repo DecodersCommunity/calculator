2cccbe84e8231a010d055270de5c00c8b9f0befa
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2022-10-04 21:35:13 +0530

    added sound

--- src/script.js
+++ src/script.js
@@ -1,19 +1,29 @@
 let string = "";
 let buttons = document.querySelectorAll('.button');
+// audio which will be played on click
+const clickAudio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
       string = eval(string);
       document.querySelector('input').value = string;
+      // playing audio
+      clickAudio.play();
     }
     else if(e.target.innerHTML == 'C'){
       string = ""
       document.querySelector('input').value = string;
+      // playing audio
+      clickAudio.play();
     }
     else{ 
     console.log(e.target)
     string = string + e.target.innerHTML;
     document.querySelector('input').value = string;
-      }
+      // playing audio
+      clickAudio.play();
+    }
   })
 })
+
